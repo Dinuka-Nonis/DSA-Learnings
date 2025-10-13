cbd3aeb126195807214f413ce4014c6271cba191
Dinuka-Nonis <dinukanonis@gmail.com>
2025-10-13 21:22:09 +0530

    Lec2-Pattern-2 and switch case integration

--- Step1-Learn the basics/Lec2/Patterns.java
+++ Step1-Learn the basics/Lec2/Patterns.java
@@ -1,11 +1,40 @@
+
+import java.util.Scanner;
+
+
+
 public class Patterns {
+
+    
     public static void main(String[] args) {
-        System.out.println("Pattern 1 - Rectangular Star Pattern");
-        for(int i = 0 ; i<5 ; i++){
-            for(int j=0 ; j < 5 ; j++){
-            System.out.print("* ");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Chose a pattern to print.");
+        System.out.println("1.Rectangular star pattern.");
+        System.out.println("2.Right-Angled Triangle Pattern");
+        int num = sc.nextInt();
+
+        switch(num) {
+            case 1 :
+            System.out.println("Pattern 1 - Rectangular Star Pattern");
+                for(int i = 0 ; i<5 ; i++){
+                    for(int j=0 ; j < 5 ; j++){
+                        System.out.print("* ");
+                    }
+                System.out.println();
             }
-            System.out.println();
+            case 2 :
+            System.out.println("Right-Angled Triangle Pattern");
+                for(int i = 0 ; i<5 ; i++){
+                    for(int j=0 ; j < i ; j++){
+
+                        System.out.print("* ");
+                    }
+
+                System.out.println();
+            }
         }
+
+        
     }
 }
