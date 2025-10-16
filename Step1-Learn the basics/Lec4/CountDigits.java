b323e7b53a513ef29ff8680e82bdb1b0deee06eb
Dinuka-Nonis <dinukanonis@gmail.com>
2025-10-16 21:50:04 +0530

    Lec4-counting digits

---
+++ Step1-Learn the basics/Lec4/CountDigits.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class CountDigits {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a value : ");
+    int num = sc.nextInt();
+        int count = 0;
+        int temp = Math.abs(num);
+
+        if (temp == 0) {
+            count = 1; // 0 has one digit
+        } else {
+            while (temp > 0) {
+                temp = temp / 10; // remove last digit
+                count++;
+            }
+        }
+    System.out.println("input :"+num);
+System.out.println("Number of digits: "+count);
+}
+}
+
