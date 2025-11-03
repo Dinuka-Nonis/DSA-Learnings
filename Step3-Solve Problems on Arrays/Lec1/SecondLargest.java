5800ec00223b3b1269d8b46db3e72c9343d19eb5
Dinuka-Nonis <dinukanonis@gmail.com>
2025-11-03 08:49:37 +0530

    Finding the second smallest value of an array without sorting

---
+++ Step3-Solve Problems on Arrays/Lec1/SecondLargest.java
@@ -0,0 +1,36 @@
+public class SecondLargest {
+    public static void main(String[] args) {
+        int arr[] = {12,19,47,26,91,19,21};
+        int n = arr.length;
+        
+        int secondSmall = secondsmallest(arr, n);
+
+        if (secondSmall == Integer.MAX_VALUE) {
+            System.out.println("No distinct second smallest element found.");
+        } else {
+            System.out.println("Second smallest element is: " + secondSmall);
+        }
+
+        
+    }
+    static int secondsmallest(int[] arr, int n){
+        if(n<2) {
+            System.out.println("Array need at least two elements to find second smallest");
+            return Integer.MAX_VALUE ;
+        }
+
+        int small = Integer.MAX_VALUE;
+        int secondsmallest = Integer.MAX_VALUE;
+
+        for(int i = 0 ; i<n;i++){
+            if(arr[i]<small){
+                secondsmallest = small;
+                small = arr[i];
+            }else if (arr[i] < secondsmallest && arr[i] != small){
+                secondsmallest = arr[i];
+            }
+        }
+        return secondsmallest;
+    }
+    
+}
