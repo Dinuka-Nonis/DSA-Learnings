971ff0c8b6437e879dfa8c7629deb12f2246aef0
Dinuka-Nonis <dinukanonis@gmail.com>
2025-11-05 08:16:30 +0530

    Lec1 - Removing duplicated values from a sorted array

--- Step3-Solve Problems on Arrays/Lec1/RemoveDuplicates.java
+++ Step3-Solve Problems on Arrays/Lec1/RemoveDuplicates.java
@@ -1,15 +1,27 @@
 public class RemoveDuplicates {
-    public int removeDuplicates(int[] nums){
-        if(nums.length == 0) return 0;
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
 
-        int i =0;
-        for(int j = 1; j<nums.length ; j++){
-            if(nums[j]!=nums[i]){
-            i++;
-            nums[i]= nums[j];
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
             }
         }
-        return i+1;
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+
+        RemoveDuplicates obj = new RemoveDuplicates();
+        int k = obj.removeDuplicates(nums);
+
+        System.out.println("Unique count = " + k);
+        System.out.print("Array after removing duplicates: ");
+        for (int x = 0; x < k; x++) {
+            System.out.print(nums[x] + " ");
+        }
     }
 }
-
