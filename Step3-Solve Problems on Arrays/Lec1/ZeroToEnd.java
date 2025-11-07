b9361fc4e4447ab80bd5b99f9a06285bc2400629
Dinuka-Nonis <dinukanonis@gmail.com>
2025-11-07 08:22:28 +0530

    if there is a zero swap it with the nextt elemant

--- Step3-Solve Problems on Arrays/Lec1/ZeroToEnd.java
+++ Step3-Solve Problems on Arrays/Lec1/ZeroToEnd.java
@@ -1,12 +1,22 @@
 public class ZeroToEnd {
     static void moveZeros(int[] arr , int n ){
+        int j = -1;
         for(int i=0 ; i<n ; i++){
-            int j = -1;
+            
             if(arr[i]==0){
                 j=i;
                 break;
             }
-            if( j == -1) return;
+        }
+            if( j == -1) return; // no zero found 
+
+            for (int i=j+1 ; i<n ;i++){
+                if(arr[i] !=0){ // if the current element is Zero
+                    int temp = arr[i]; // swap it with arr[j]
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    j++;
+                }     
         }
     
     }
