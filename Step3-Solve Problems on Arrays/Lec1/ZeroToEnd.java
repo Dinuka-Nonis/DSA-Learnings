d19eebc56ffcb354c5345009d1970ef10a28ec2d
Dinuka-Nonis <dinukanonis@gmail.com>
2025-11-07 08:16:05 +0530

    MoveZeros to the end- find if there are zeros

---
+++ Step3-Solve Problems on Arrays/Lec1/ZeroToEnd.java
@@ -0,0 +1,13 @@
+public class ZeroToEnd {
+    static void moveZeros(int[] arr , int n ){
+        for(int i=0 ; i<n ; i++){
+            int j = -1;
+            if(arr[i]==0){
+                j=i;
+                break;
+            }
+            if( j == -1) return;
+        }
+    
+    }
+}
