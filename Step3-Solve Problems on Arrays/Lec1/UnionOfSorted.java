f855913559c56664edb16ddcdd43231a002fbd58
Dinuka-Nonis <dinukanonis@gmail.com>
2025-11-09 09:35:21 +0530

    union of a sorted array - manin

--- Step3-Solve Problems on Arrays/Lec1/UnionOfSorted.java
+++ Step3-Solve Problems on Arrays/Lec1/UnionOfSorted.java
@@ -22,11 +22,20 @@ public class UnionOfSorted {
             Union.add(arr1[i]);
             i++;
         }
-        while(j<n){
+        while(j<m){
             if(Union.get(Union.size()-1) != arr2[j])
             Union.add(arr2[j]);
             j++;
         }
         return Union;
     }
+    public static void main(String args[]) {
+  int n = 10, m = 7;
+  int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int arr2[] = {2, 3, 4, 4, 5, 11, 12};
+  ArrayList<Integer> Union = FindUnion(arr1, arr2, n, m);
+  System.out.println("Union of arr1 and arr2 is ");
+  for (int val: Union)
+    System.out.print(val+" ");
+}
 }
