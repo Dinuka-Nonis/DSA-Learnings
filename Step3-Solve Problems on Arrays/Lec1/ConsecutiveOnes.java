96131a80f87bfb80651724fd9cdc1986e20be386
Dinuka-Nonis <dinukanonis@gmail.com>
2025-11-11 11:20:57 +0530

    function for compare and return the maximum number

--- Step3-Solve Problems on Arrays/Lec1/ConsecutiveOnes.java
+++ Step3-Solve Problems on Arrays/Lec1/ConsecutiveOnes.java
@@ -1,3 +1,18 @@
 public class ConsecutiveOnes {
-    
+    static int ConsecOnes(int[] arr, int n) {
+        int maxCount = 0 ;
+        int currentCount = 0; 
+
+        for(int i = 0 ; i<n ;i++){
+            if(arr[i] == 1){
+                currentCount++;
+            } else
+            currentCount = 0;
+
+            if(maxCount < currentCount){
+                maxCount=currentCount;
+            }
+        }
+        return maxCount;
+    }
 }
