f1a31f5867f41d23de4fabb3430092cbe1072056
Dinuka-Nonis <dinukanonis@gmail.com>
2025-11-12 08:33:13 +0530

    Approach explained

--- Step3-Solve Problems on Arrays/Lec1/NumberThatAppersOnce.java
+++ Step3-Solve Problems on Arrays/Lec1/NumberThatAppersOnce.java
@@ -1,5 +1,13 @@
 public class NumberThatAppersOnce {
 
+    // Approach Explanation:
+// We use the XOR (^) operator to find the number that appears only once.
+// XOR has two key properties:
+// 1. a ^ a = 0  → XOR of the same numbers is 0.
+// 2. a ^ 0 = a  → XOR of any number with 0 gives the number itself.
+// Since all numbers except one appear twice, their XOR becomes 0.
+// Finally, XORing all elements together leaves only the single unique number.
+
     static int XORfunc(int[] arr, int n){
         int ans = 0 ; 
         for(int i = 0; i<n ; i++){
