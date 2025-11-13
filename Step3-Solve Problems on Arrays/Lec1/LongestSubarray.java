2cf8d2ee85bb6afca3664f92ca7412da8bd9b761
Dinuka-Nonis <dinukanonis@gmail.com>
2025-11-13 21:21:03 +0530

    main program

--- Step3-Solve Problems on Arrays/Lec1/LongestSubarray.java
+++ Step3-Solve Problems on Arrays/Lec1/LongestSubarray.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 
 public class LongestSubarray {
 
-    static int SubArray(int[] arr, int n, int k) {
+    static int longestSubarray(int[] arr, int n, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
         int prefixSum = 0;
         int maxLen = 0;
@@ -29,6 +29,21 @@ public class LongestSubarray {
 
         return maxLen;
     }
+public static void main(String[] args) {
+        int[] arr1 = {2, 3, 5};
+        int k1 = 5;
+        System.out.println("Longest subarray length (Example 1) = " + longestSubarray(arr1, arr1.length, k1));
+        // Expected output: 2
 
+        int[] arr2 = {2, 3, 5, 1, 9};
+        int k2 = 10;
+        System.out.println("Longest subarray length (Example 2) = " + longestSubarray(arr2, arr2.length, k2));
+        // Expected output: 3
+
+        int[] arr3 = {1, 2, 3, 7, 5};
+        int k3 = 12;
+        System.out.println("Longest subarray length (Example 3) = " + longestSubarray(arr3, arr3.length, k3));
+        // Expected output: 3 ([2,3,7])
+    }
     
 }
