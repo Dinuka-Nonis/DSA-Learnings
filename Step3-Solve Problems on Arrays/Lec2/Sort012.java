7834891afdf3f309d79d6a8f740ae9d382a87511
Dinuka-Nonis <dinukanonis@gmail.com>
2025-11-16 21:06:01 +0530

    complete function

--- Step3-Solve Problems on Arrays/Lec2/Sort012.java
+++ Step3-Solve Problems on Arrays/Lec2/Sort012.java
@@ -1,3 +1,25 @@
 public class Sort012 {
-    
+    static int[] Sort(int[] arr , int n ){
+        int low = 0;
+        int mid = 0;
+        int high = n-1;
+
+        while(mid <=high) {
+            if(arr[mid] == 0){
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                mid++;
+                low++;
+            }else if(arr[mid] == 1){
+                mid++;
+            }else {
+                int temp = arr[high];
+                arr[high] = arr[mid];
+                arr[mid] = temp ;
+                high--;
+            }
+        }
+        return arr;
+    }
 }
