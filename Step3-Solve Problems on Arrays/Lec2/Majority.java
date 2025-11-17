a152dbf7be1f800ca671251b598849822d19ca73
Dinuka-Nonis <dinukanonis@gmail.com>
2025-11-17 15:10:38 +0530

    using Moore's Voting algorithm'

--- Step3-Solve Problems on Arrays/Lec2/Majority.java
+++ Step3-Solve Problems on Arrays/Lec2/Majority.java
@@ -16,9 +16,27 @@ public class Majority {
         }
         return -1;
     }
+    static int MooreVoting(int[] arr , int n){
+        int count = 0 ;
+        int element = 0;
+
+        for(int i=0;i<n;i++){
+            if(count == 0){
+            element = arr[i];}
+            if(arr[i] == element){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return element;
+
+    }
     public static void main(String[] args) {
-        int[] arr = {2,2,1,1,1,2,2};
+        int[] arr = {2,2,2,2,1,1,1};
         int n = arr.length;
         System.out.println(majority(arr, n));
+
+        System.out.println(MooreVoting(arr, n));
     }
 }
