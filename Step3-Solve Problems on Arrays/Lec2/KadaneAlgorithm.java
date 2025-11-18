4e031c816bf4db0cc1300141c6ee624aaf631f93
Dinuka-Nonis <dinukanonis@gmail.com>
2025-11-18 22:34:50 +0530

    main added

--- Step3-Solve Problems on Arrays/Lec2/KadaneAlgorithm.java
+++ Step3-Solve Problems on Arrays/Lec2/KadaneAlgorithm.java
@@ -30,5 +30,10 @@ public class KadaneAlgorithm {
         System.out.println("]");
     }
 
-    
+    public static void main(String[] args) {
+        int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
+        int n = arr.length;
+
+        kadane(arr, n);
+    }
 }
