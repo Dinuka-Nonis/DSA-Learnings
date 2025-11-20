4167facb2a4246c0956f17b853ecbb782f0c5168
Dinuka-Nonis <dinukanonis@gmail.com>
2025-11-20 23:36:01 +0530

    main for the stock profit

--- Step3-Solve Problems on Arrays/Lec2/StockProfit.java
+++ Step3-Solve Problems on Arrays/Lec2/StockProfit.java
@@ -12,5 +12,12 @@ public class StockProfit {
         }
         return maxProfit;
     }
+    public static void main(String[] args) {
+        int[] prices1 = {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices1)); // Output: 5
+
+        int[] prices2 = {7,6,4,3,1};
+        System.out.println(maxProfit(prices2)); // Output: 0
+    }
     
 }
