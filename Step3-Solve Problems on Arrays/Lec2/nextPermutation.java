8109fc4c4bbcc04b1c2bbeb69cf3045f52f5be35
Dinuka-Nonis <dinukanonis@gmail.com>
2025-11-27 22:02:05 +0530

    Problem explained

--- Step3-Solve Problems on Arrays/Lec2/nextPermutation.java
+++ Step3-Solve Problems on Arrays/Lec2/nextPermutation.java
@@ -1,5 +1,22 @@
+// Problem:
+// Given an array of integers, rearrange the numbers to form the next
+// lexicographically greater permutation. This means we must create the
+// next arrangement that would appear if all permutations were sorted.
+//
+// If the current arrangement is already the highest possible permutation
+// (e.g., the array is in descending order), then we return the lowest
+// possible arrangement by sorting the array in ascending order.
+//
+// Example:
+// Input:  [1, 3, 2]
+// Output: [2, 1, 3]   // This is the next permutation after 1 3 2
+//
+// Input:  [3, 2, 1]
+// Output: [1, 2, 3]   // Already the largest, so return smallest
+
+
 public class nextPermutation {
     public void nextPermutation(int[] nums){
-        
+
     }
 }
