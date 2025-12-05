9c9225ba54f5a00e5833ab08ae6bffc896be801e
Dinuka-Nonis <dinukanonis@gmail.com>
2025-12-05 09:12:10 +0530

    Upper Bound problem

---
+++ Step4-Binary Search/Lec1/UpperBound.java
@@ -0,0 +1,19 @@
+public class UpperBound {
+    public static int UpperBound(int[] arr, int x)   {
+        int n= arr.length;
+        int low = 0,high = n-1;
+        int ans = n;
+
+        while(low <= high){
+            int mid = low + (high-low)/2;
+
+            if(arr[mid] > x){
+                ans= mid;
+                high = mid-1;
+            } else {
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+}
