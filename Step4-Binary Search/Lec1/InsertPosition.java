9e67ceb55852fa70305a8d6010bb57837cb079f5
Dinuka-Nonis <dinukanonis@gmail.com>
2025-12-16 14:41:59 +0530

    Problem and approach simplified

--- Step4-Binary Search/Lec1/InsertPosition.java
+++ Step4-Binary Search/Lec1/InsertPosition.java
@@ -1,6 +1,21 @@
 
 import java.util.Scanner;
+ /*
+     * PROBLEM:
+     * Given a sorted array of distinct integers and a target value x,
+     * find the index of x if it exists.
+     * If x does not exist, return the index where it should be inserted
+     * so that the array remains sorted.
+     */
 
+    /*
+     * APPROACH:
+     * Since the array is sorted, we use Binary Search.
+     * - If the target is found, return its index.
+     * - If not found, the position where binary search stops (low)
+     *   gives the correct insertion index.
+     * This approach runs in O(log n) time.
+     */
 public class InsertPosition {
     public static int searchInsert(int[]arr, int x) {
         int low = 0, high = arr.length-1;
