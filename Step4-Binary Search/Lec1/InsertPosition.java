e8a7a1c984be91d969362eff7db916473f25fec6
Dinuka-Nonis <dinukanonis@gmail.com>
2025-12-16 14:41:01 +0530

    Main function for the insert position problem

--- Step4-Binary Search/Lec1/InsertPosition.java
+++ Step4-Binary Search/Lec1/InsertPosition.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 public class InsertPosition {
     public static int searchInsert(int[]arr, int x) {
         int low = 0, high = arr.length-1;
@@ -13,5 +16,17 @@ public class InsertPosition {
                 high = mid-1;
         }
         return low;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,7};
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the value to search: ");
+        int x = sc.nextInt();
+
+        int result = searchInsert(arr, x);
+        System.out.println("Result index: " + result);
+
+        sc.close();
     }
 }
