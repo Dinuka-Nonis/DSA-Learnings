e207c17a3d7c258deda0d017b93d3f7cfb68d695
Dinuka-Nonis <dinukanonis@gmail.com>
2025-12-17 08:06:53 +0530

    Main function created with and example - answer 4 7

--- Step4-Binary Search/Lec1/FloorAndCeiling.java
+++ Step4-Binary Search/Lec1/FloorAndCeiling.java
@@ -39,5 +39,10 @@ public class FloorAndCeiling {
         }
         System.out.println(floor+ " " + ceil);
     }
-    
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 4, 7, 8, 10};
+        int x = 5;
+
+        findFloorCeil(arr, x);
+    }
 }
