8b6bf50ec55ea5fbb2aeb2d2d8c1684bef1d3c43
Dinuka-Nonis <dinukanonis@gmail.com>
2025-12-26 21:48:39 +0530

    Main function

--- Step4-Binary Search/Lec1/RotatedSort2.java
+++ Step4-Binary Search/Lec1/RotatedSort2.java
@@ -35,6 +35,18 @@ public class RotatedSort2 {
 
         return false; // Not found
     }
+    public static void main(String[] args) {
+        int[] arr = {7, 8, 1, 2, 3, 3, 3, 4, 5, 6};
+        int k = 3;
+
+        RotatedSort2 obj = new RotatedSort2();
+        boolean ans = obj.searchInRotatedSortedArrayII(arr, k);
+
+        if (ans)
+            System.out.println("Target is present in the array.");
+        else
+            System.out.println("Target is not present.");
+    }
 
     
 }
