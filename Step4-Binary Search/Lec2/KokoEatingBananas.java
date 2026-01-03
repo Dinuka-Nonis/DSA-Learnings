67b0740b7c38377b6b04fbe16d2d92ff5d0746ef
Dinuka-Nonis <dinukanonis@gmail.com>
2026-01-03 22:41:18 +0530

    koko eating bananas problem - minimum eating speed

--- Step4-Binary Search/Lec2/KokoEatingBananas.java
+++ Step4-Binary Search/Lec2/KokoEatingBananas.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class KokoEatingBananas {
     private int calculateTotalHours(int[] piles, int speed) {
         int totalH = 0;
@@ -5,5 +7,30 @@ public class KokoEatingBananas {
             totalH += (int)Math.ceil((double)bananas / speed);
         }
         return totalH;
+    }
+     public int minEatingSpeed(int[] piles, int h) {
+        // Find maximum element
+        int maxPile = Arrays.stream(piles).max().getAsInt();
+
+        // Initialize low and high pointers
+        int low = 1, high = maxPile;
+        int ans = maxPile;
+
+        // Binary search on answer space
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int totalH = calculateTotalHours(piles, mid);
+
+            // If possible, try smaller speed
+            if (totalH <= h) {
+                ans = mid;
+                high = mid - 1;
+            }
+            // Otherwise, try larger speed
+            else {
+                low = mid + 1;
+            }
+        }
+        return ans;
     }
 }
