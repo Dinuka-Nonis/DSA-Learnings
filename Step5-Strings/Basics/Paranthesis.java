bd05e205115b0c6847ac378472f4d9704ec09634
Dinuka-Nonis <dinukanonis@gmail.com>
2026-01-14 09:45:36 +0530

    main func

--- Step5-Strings/Basics/Paranthesis.java
+++ Step5-Strings/Basics/Paranthesis.java
@@ -16,4 +16,12 @@ public class Paranthesis {
         }
         return result.toString();
     }
+    public static void main(String[] args) {
+        String s = "(()())(())";
+        Paranthesis sol = new Paranthesis();
+
+        String ans = sol.removeOuterParentheses(s);
+
+        System.out.println("The result is: "+ ans);
+    }
 }
